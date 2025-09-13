b37c8d4302dbd80db5e594f8300c862ceb8b8e42
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2025-09-14 00:02:48 +0200

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,4 +1,5 @@
 cd c:\na\v3
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
-powershell.exe C:\NA\V3\Setup_gui.exe
+powershell.exe C:\NA\V3\Gui_runner.exe
 exit
+
