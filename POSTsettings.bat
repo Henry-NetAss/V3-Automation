32561af09e2c89568b27968f808af7d303f7478e
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2025-09-14 10:43:25 +0200

    Update POSTsettings.bat

--- POSTsettings.bat
+++ POSTsettings.bat
@@ -1,10 +1,11 @@
 cd c:\na\v3
 powershell -command "& {Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force}"
-powershell.exe C:\NA\Installation\remove_printers.ps1
-powershell.exe c:\na\Installation\Desktop_Shortcuts.ps1
-powershell.exe c:\na\installation\taskbaricons.ps1
-powershell.exe c:\na\Installation\DisableSearchBoxSuggestion.ps1
-powershell.exe C:\NA\Installation\Region_Settings.ps1
-powershell.exe C:\NA\Installation\updates.ps1
+powershell.exe C:\NA\V3\remove_printers.ps1
+powershell.exe c:\na\V3\Desktop_Shortcuts.ps1
+powershell.exe c:\na\V3\taskbaricons.ps1
+powershell.exe c:\na\V3\DisableSearchBoxSuggestion.ps1
+powershell.exe C:\NA\V3\Region_Settings.ps1
+powershell.exe C:\NA\V3\updates.ps1
 exit
+
 
