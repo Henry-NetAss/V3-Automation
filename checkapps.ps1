b79f289c3c5bd849773797a26846bad4177c1f57
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2025-09-15 10:39:57 +0200

    Update checkapps.ps1

--- checkapps.ps1
+++ checkapps.ps1
@@ -1,48 +1,13 @@
 # Anydesk
-$anydeskExePath = "C:\Program Files (x86)\AnyDesk\AnyDesk.exe"
+$esetExePath = "C:\Program Files\ESET\ESET Security\ecmds.exe"
 
-if (Test-Path $anydeskExePath) {
-    Write-Host "AnyDesk is installed."
+if (Test-Path $esetExePath) {
+    set-location C:\na\v3
+    ./PROTECTAgentInstaller.bat
 } else {
-    write-host "Anydesk is not installed"
-    Write-Output y|winget install --id anydesk.anydesk
+    write-host "ESET is not installed"
 }
-# Chrome
+exit
 
-$ChromeExePath = "C:\Program Files\Google\Chrome\Application\chrome.exe"
-
-if (Test-Path $ChromeExePath) {
-    Write-Host "Chrome is installed."
-} else {
-    $Chromeexepath1 = "$env:USERPROFILE\AppData\Local\Google\Chrome\Application\chrome.exe"
-
-    if (test-path $chromeexepath1) {
-        write-host "Chrome is installed"
-    } else { write-host "Chrome is not inistalled"
-            write-output y|winget install --id Google.Chrome --Location "C:\Program Files\Google"
-        }
-    }
-
-# TS Print
-
-$TSPrintExePath = "C:\Program Files (x86)\TerminalWorks\TSPrint\showoptions.exe"
-
-if (Test-Path $TSPrintExePath) {
-    Write-Host "TS Print is installed."
-} else {
-    write-host "TS Print is not installed"
-    wite-output y|winget install --id TerminalWorks.TSPrintClient
-}
-
-# Adobe
-
-$AdobeExePath = "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe"
-
-if (Test-Path $AdobeExePath) {
-    Write-Host "Adobe is installed."
-
-} else { write-host "Adobe is not installed"
-Write-Output y|winget install --id Adobe.Acrobat.Reader.32-bit --silent
-}
 
 
