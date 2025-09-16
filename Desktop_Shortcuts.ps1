a56a1dd4e7c5cfb970b75d70aa739d558978eeff
Henry-NetAss <henry.cloete1111@gmail.com>
2025-09-16 16:26:49 +0200

    Update Desktop_Shortcuts.ps1

--- Desktop_Shortcuts.ps1
+++ Desktop_Shortcuts.ps1
@@ -1,3 +1,4 @@
+copy-item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Excel.lnk" -Destination "C:\Users\Public\Desktop\Excel.lnk" -Recurse -Force
 copy-item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Word.lnk" -Destination "C:\Users\Public\Desktop\Word.lnk" -Recurse -Force
 copy-item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook (Classic).lnk" -Destination "C:\Users\Public\Desktop\Outlook.lnk" -Recurse -Force
 copy-item -Path "$env:USERPROFILE\appdata\local\google\chrome\application\chrome.exe" -Destination "c:\na\installation\chrome.lnk" -Recurse -Force
