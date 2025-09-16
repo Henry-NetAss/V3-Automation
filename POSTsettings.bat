0c567409e0f5202589e275b4af7791c4b4281479
Henry-NetAss <henry.cloete1111@gmail.com>
2025-09-16 16:28:00 +0200

    Update POSTsettings.bat

--- POSTsettings.bat
+++ POSTsettings.bat
@@ -4,6 +4,7 @@ powershell.exe C:\NA\V3\Checkapps.ps1
 powershell.exe C:\NA\V3\remove_printers.ps1
 powershell.exe c:\na\V3\Desktop_Shortcuts.ps1
 # powershell.exe c:\na\V3\Officefonts.ps1
+powershell.exe c:\na\V3\Generic.exe /quiet
 powershell.exe c:\na\V3\taskbaricons.ps1
 powershell.exe c:\na\V3\DisableSearchBoxSuggestion.ps1
 powershell.exe c:\na\V3\Him.exe /quiet
