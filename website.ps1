4f5b9a41b8565ca0a0d82943cbe0c0070fadf602
Henry-NetAss <142622280+Henry-NetAss@users.noreply.github.com>
2025-09-24 15:01:40 +0200

    Delete website.ps1

--- website.ps1
+++
@@ -1,7 +0,0 @@
-start microsoft-edge:http://networkassociates.co.za 
-$wshell = New-Object -ComObject wscript.shell;
-$wshell.AppActivate('Google - Microsoft Edge')
-Sleep 2
-$wshell.SendKeys('(%(" "))')
-Sleep 2
-$wshell.SendKeys('(x)')
