9febb54a5c857c93f34e0131a883dd0634c0d4a3
Henry-NetAss <henry.cloete1111@gmail.com>
2025-09-25 13:12:42 +0200

    test

--- Dell Support Assist.ps1
+++
@@ -1,4 +0,0 @@
-Set-location "C:\na\V3"
-Invoke-WebRequest -Uri "https://downloads.dell.com/serviceability/catalog/SupportAssistBusinessInstaller.exe" -OutFile "C:\NA\V3\DELLSA.exe"
-start-sleep -seconds 15
-./dellsa.exe /quiet
